dc3e499c798a7a5122cd045beea195f46862bfab
Dami <damianhettich@gmail.com>
2022-09-03 18:33:47 -0400

    created a poll-question-choice model for orm

--- plebiscito/poll/models.py
+++ plebiscito/poll/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    name = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Question(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    publication_date = models.DateTimeField("date published")
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
