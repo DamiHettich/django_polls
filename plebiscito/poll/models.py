518ce4144941a18f1ad021864f3894c262ac5576
Dami <damianhettich@gmail.com>
2022-09-04 10:49:42 -0300

    last changes on str methods in models

--- plebiscito/poll/models.py
+++ plebiscito/poll/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 import datetime as dt
+from django.utils import timezone
 
 # Create your models here.
 class Poll(models.Model):
     name = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    publication_date = models.DateTimeField("date published", default=timezone.now())
 
-
+    def is_recently_published(self):
+        return self.publication_date + dt.timedelta(days=1) > dt.datetime.now()
 
     def __str__(self):
         return f"Name: {self.name}, Total Votes: {self.votes}"
@@ -18,18 +21,15 @@ class Poll(models.Model):
 class Question(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
-    publication_date = models.DateTimeField("date published")
-    
-    def is_recently_published(self):
-        return self.publication_date + dt.timedelta(days=1) > dt.datetime.now()
-    
-    def __str__(self):
-        return f"{self.question_text} \nPublishedAt: {self.publication_date}"
 
+
+    def __str__(self):
+        return f"Q: {self.question_text}, Poll ID: {self.poll}"
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    
+    def __str__(self):
+        return f"Text: {self.choice_text}, Votes: {self.votes}, Q ID: {self.question}"
