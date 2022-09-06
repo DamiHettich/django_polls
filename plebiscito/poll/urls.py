7f1f4ad6f265207206609e54a09bba1dba445178
Dami <damianhettich@gmail.com>
2022-09-06 00:29:07 -0300

    created results, details, vote views and urls

--- plebiscito/poll/urls.py
+++ plebiscito/poll/urls.py
@@ -2,5 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:poll_id>/", views.poll_details, name="poll_details"),
+    path("<int:poll_id>/results", views.poll_results, name="poll_results"),
+    path("questions/<int:question_id>/", views.question_details, name="question_details"),
+    path("questions/<int:question_id>/results", views.question_results, name="question_results"),
+    path("questions/<int:question_id>/vote", views.question_vote, name="votes"),
+
 ]
--- plebiscito/poll/views.py
+++ plebiscito/poll/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from .models import Question, Poll, Choice
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Hello World </h1>')
+    return HttpResponse('<h1> Página principal Aplicación Encuestas </h1>')
+
+def poll_details(request, poll_id):
+    poll_obj = get_object_or_404(Poll, pk = poll_id)
+    questions = poll_obj.question_set.all()
+    print(questions)
+    my_context = {
+        'poll':poll_obj,
+        'question_list':questions
+    }
+    return render(request, 'polls/details.html', context = my_context)
+
+def poll_results(request, poll_id):
+    return HttpResponse(f'<h1> Resultados para la Encuesta {poll_id} </h1>')
+
+def question_details(request, question_id):
+    return HttpResponse(f'<h1> Detalle pregunta:{question_id} </h1>')
+
+def question_results(request, question_id):
+    return HttpResponse(f'<h1> Resultados para la pregunta {question_id} </h1>')
+
+def question_vote(request, question_id):
+    return HttpResponse(f'<h1> Votando en {question_id} </h1>')
